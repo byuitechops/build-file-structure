268950114999fe4cf5cce5158f0a51212e50ffcd
Zachary Williams <wil11063@byui.edu>
2018-06-15 10:57:33 -0600

    Basic working version

---
+++ fileType.js
@@ -0,0 +1,108 @@
+/*******************************************************************
+ * Takes a single filepath and returns the file type as a string.
+ * File types are:
+ * document
+ * video
+ * image
+ * audio
+ * template (for conversion purposes)
+ ******************************************************************/
+
+const path = require('path');
+
+var webExtensions = [
+    '.js',
+    '.css',
+];
+
+var documentExtensions = [
+    '.html',
+    '.doc',
+    '.docx',
+    '.pdf',
+    '.xls',
+    '.xlsx',
+    '.csv',
+    '.odt',
+    '.ods',
+    '.txt',
+    '.dat',
+    '.log',
+    '.mdb',
+    '.sav',
+    '.sql',
+    '.tar',
+    '.xlr',
+    '.wpd',
+    '.wks',
+    '.wps',
+    '.xlsm',
+    '.rtf',
+    '.xps',
+    '.ppt',
+    '.pptx',
+    '.pps',
+    '.xml',
+    '.ttf',
+];
+
+var imageExtensions = [
+    '.png',
+    '.jpeg',
+    '.gif',
+    '.bmp',
+    '.ai',
+    '.ico',
+    '.jpg',
+    '.ps',
+    '.psd',
+    '.svg',
+    '.tif',
+    '.tiff',
+    '.pub',
+];
+
+var videoExtensions = [
+    '.avi',
+    '.wmv',
+    '.mpg',
+    '.mpeg',
+    '.swf',
+    '.mov',
+    '.mp4',
+];
+
+var audioExtensions = [
+    '.aif',
+    '.cda',
+    '.mid',
+    '.midi',
+    '.mp3',
+    '.wav',
+    '.ogg',
+    '.wma',
+    '.wpl',
+];
+
+var templateExtensions = [
+    'dashboard.jpg',
+    'homeImage.jpg',
+    'courseBanner.jpg'
+];
+
+// Will be deleted
+var deletableExtensions = [
+    '.slk'
+];
+
+module.exports = (fileName) => {
+    var ext = path.extname(fileName.toLowerCase());
+    if (templateExtensions.includes(ext)) return 'template';
+    if (webExtensions.includes(ext)) return 'documents';
+    if (documentExtensions.includes(ext)) return 'documents';
+    if (imageExtensions.includes(ext)) return 'media';
+    if (videoExtensions.includes(ext)) return 'media';
+    if (audioExtensions.includes(ext)) return 'media';
+    if (deletableExtensions.includes(ext)) return 'documents';
+    return null;
+};
---
+++ main.js
@@ -0,0 +1,136 @@
+const canvas = require('canvas-api-wrapper');
+const fileType = require('./fileType.js');
+const he = require('he');
+
+module.exports = (course, stepCallback) => {
+
+    var templateFiles = [
+        'dashboard.jpg',
+        'homeImage.jpg',
+        'courseBanner.jpg'
+    ];
+
+    (async () => {
+        return new Promise(async (resolve, reject) => {
+            var canvasCourse = canvas.getCourse(course.info.canvasOU);
+            await canvasCourse.files.get();
+            await canvasCourse.folders.get();
+            let topFolder = canvasCourse.folders.find(folder => folder.name === 'course files');
+
+            // Create the three main folders: documents, media, template
+            if (course.settings['Create three main folders']) {
+
+                let documents = canvasCourse.folders.find(folder => folder.name === 'documents');
+                let media = canvasCourse.folders.find(folder => folder.name === 'media');
+                let template = canvasCourse.folders.find(folder => folder.name === 'template');
+
+                if (!documents) documents = await canvasCourse.folders.create({name: 'documents', parent_folder_id: topFolder.id});
+                if (!media) media = await canvasCourse.folders.create({name: 'media', parent_folder_id: topFolder.id});
+                if (!template) template = await canvasCourse.folders.create({name: 'template', parent_folder_id: topFolder.id});
+
+                var mainThree = {
+                    documents,
+                    media,
+                    template
+                }
+
+                course.log('Folders Created', {
+                    'Name': 'documents'
+                });
+                course.log('Folders Created', {
+                    'Name': 'media'
+                });
+                course.log('Folders Created', {
+                    'Name': 'template'
+                });
+            }
+
+            // Move all files into each of the three main folders, then delete all other folders
+            if (course.settings['Move files into three main folders'] && course.settings['Create three main folders']) {
+                canvasCourse.files.forEach(file => {
+                    var type = fileType(file.display_name);
+                    if (templateFiles.includes(file.display_name)) {
+                        type = 'template';
+                    }
+
+                    if (mainThree[type]) {
+                        file.display_name = he.encode(file.display_name);
+                        file.on_duplicate = 'rename';
+                        file.parent_folder_id = '' + mainThree[type].id;
+                        course.log('Files Moved', {
+                            'Name': file.display_name,
+                            'New Location': type
+                        });
+                    }
+                });
+
+                // Update the files to relocate them
+                await canvasCourse.files.update();
+
+                // Re-retrieve the folders, since we need updated file counts
+                await canvasCourse.folders.get();
+
+                var folderNames = [
+                    'documents',
+                    'media',
+                    'template',
+                    'archive',
+                    'course files'
+                ];
+
+                for (var x = 0; x < canvasCourse.folders.length; x++) {
+                    if (folderNames.every(name => canvasCourse.folders[x].name !== name) && canvasCourse.folders[x].files_count === 0) {
+                        try {
+                            await canvas.delete(`/api/v1/folders/${canvasCourse.folders[x].id}?force=true`);
+                            course.log('Folders Deleted', {
+                                'Name': canvasCourse.folders[x].name
+                            });
+                        } catch (e) {
+                            console.log(canvasCourse.folders[x]);
+                            console.error(e);
+                        }
+                    }
+                }
+            }
+
+            // Create the archive folder, and move all unused files into it
+            if (course.settings['Create Archive and archive unused files']) {
+                let archive = await canvasCourse.folders.create({name: 'archive', parent_folder_id: topFolder.id});
+                canvasCourse.files.forEach(file => {
+                    if (course.info.unusedFiles & course.info.unusedFiles.includes(file.display_name)) {
+                        file.display_name = he.encode(file.display_name);
+                        file.on_duplicate = 'rename';
+                        file.parent_folder_id = archive.id;
+                        course.log('Files Archived', {
+                            'Name': file.display_name,
+                            'New Location': type
+                        });
+                    }
+                });
+                await canvasCourse.files.update();
+            }
+
+            // Delete all unused files
+            if (course.settings['Delete unused files']) {
+                for (var x = 0; x < canvasCourse.files.length; x++) {
+                    if (course.info.unusedFiles & course.info.unusedFiles.includes(canvasCourse.files[x].display_name)) {
+                        await canvasCourse.files[x].delete();
+                        course.log('Unused Files Deleted', {
+                            'Name': canvasCourse.files[x].display_name
+                        });
+                    }
+                }
+            }
+
+            resolve();
+        });
+    })()
+        .then(() => {
+            stepCallback(null, course);
+        })
+        .catch(e => {
+            course.error(e);
+            stepCallback(null, course);
+        })
+
+};
